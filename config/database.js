13d0b90067b81eb4fdbe92af3c35d3906f0fab4f
mmostefaoui <m.mostefaoui@gmail.com>
2017-04-05 07:39:05 +0200

    use remote mongodb service

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 
 module.exports = {
     'secret' : 'my super admin secret',
-    'database': 'mongodb://oteuser:password@localhost:27017/pjdb'
+    //'database': 'mongodb://oteuser:password@localhost:27017/pjdb'
+    'database': 'mongodb://oteuser:password@ds151820.mlab.com:51820/pjdb'
 };
